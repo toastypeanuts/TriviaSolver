13d124b6f4831ec8ae094615a3dfdaeabdd143c4
Mark Joh <markjoh@umich.edu>
2019-03-10 21:58:37 -0400

    updated old code

--- googSearch.py
+++ googSearch.py
@@ -29,16 +29,16 @@ def not_score (answer_count, answerSplit):
 
         return (final_answer, confidence)
 
-
+#tiebreaker if simple and advanced dont return a high enough confidence level
 def tiebreaker(question, all_text, answerSplit, not_type):
     
     print(question)
     
     answer_count = [0, 0, 0, 0]
     counter = 0
-    #print('...')
-    #print('Confidence level too low...')
-    #print('Running tiebreaker search...')
+    print('...')
+    print('Confidence level too low...')
+    print('Running tiebreaker search...')
 
     engine = inflect.engine()
 
@@ -88,13 +88,13 @@ def tiebreaker(question, all_text, answerSplit, not_type):
     f.write('\nFinal answer is: \n'+ str(final_answer+1) + '.) ' + answerSplit[final_answer]+'\n')
     f.close()
 
+#advanced search using keywords from question instead of just whole question
 def advancedSearch(question, answerSplit, not_type):
     answer_count = [0, 0, 0, 0]
     counter = 0
-    #print('...')
-    #print('Confidence level too low...')
-    #print('Running advanced search...')
-
+    print('...')
+    print('Confidence level too low...')
+    print('Running advanced search...')
 
     #extract important words out of question and scrapes web
     question = keywords(question)
@@ -107,7 +107,6 @@ def advancedSearch(question, answerSplit, not_type):
     engine = inflect.engine()
 
     #Advanced search
-
     #ignores common punctuation
     for data in all_text:
         data_strip = [x.strip(string.punctuation) for x in (data.text).split()]
@@ -118,7 +117,6 @@ def advancedSearch(question, answerSplit, not_type):
                     #Compares words regardless of plurality ..
                     if counter == 3:
                         counter = 0
-
                     if(engine.compare(word.upper(), numWords.upper()) and already_counted == False):
                         already_counted = True
                         answer_count[counter] += 1
@@ -128,13 +126,10 @@ def advancedSearch(question, answerSplit, not_type):
 
     #figuring out final answer and confidence
 
-
-
     if (not_type == False):
         confidence = answer_count[0] - max(answer_count[1], answer_count[2])
         final_answer = 0
         
-
         if(answer_count[1] > answer_count[final_answer]):
             confidence = answer_count[1] - answer_count[final_answer]
             final_answer = 1
@@ -162,6 +157,7 @@ def advancedSearch(question, answerSplit, not_type):
         f.write('Confidence level is {}'.format(round(confidence/3, 2))+'\n')
         f.write('\nFinal answer is: \n'+ str(final_answer+1) + '.) ' +  answerSplit[final_answer]+'\n')
         f.close()
+
 #simple google search
 def simpleSearch(question, all_text, answerSplit, not_type): #--------------------------------------------------------------
     answer_count = [0, 0, 0, 0]
@@ -216,39 +212,7 @@ def simpleSearch(question, all_text, answerSplit, not_type): #------------------
         f.write('\nFinal answer is: \n'+ str(final_answer+1) + '.) ' +  answerSplit[final_answer]+'\n')
         f.close()
 
-
-
-
-#--------------------------------------- Splits text into questions and analyzes answers -----------------------------------------------
-def findResults(text):
-    question = text.split('?')[0]
-    questionSplit = question.split()
-
-    answers = text.split('?')[1]
-    answerSplit = answers.split('\n')
-    
-    #filter empty strings from lists
-    questionSplit = filter(None, questionSplit)
-    answerSplit = list(filter(None,answerSplit))
-
-
-    print(' '.join(questionSplit))
-    print(', '.join(answerSplit))
-    print(' ')
-
-    #language_analysis(question)
-
-
-    #scraping google web search and returning into all_text variable
-    #runs simplesearch algo
-    a = Analysis(question)
-    all_text = a.run()
-
-    not_type = False
-    if 'not'.upper() in question:
-        not_type = True
-    simpleSearch(question, all_text, answerSplit, not_type)
-
+#strips nouns verbs adjectives from text
 def keywords(question):
     text = ''.join(question)
     words = word_tokenize(text)
@@ -262,6 +226,31 @@ def keywords(question):
     return extractedWords
     #print(simplifiedWords)
 
+#--------------------------------------- Splits text into questions and analyzes answers -----------------------------------------------
+def findResults(text):
+    question = text.split('?')[0]
+    questionSplit = question.split()
+
+    answers = text.split('?')[1]
+    answerSplit = answers.split('\n')
+    
+    #filter empty strings from lists
+    questionSplit = filter(None, questionSplit)
+    answerSplit = list(filter(None,answerSplit))
+
+    print(' '.join(questionSplit))
+    print(', '.join(answerSplit))
+    print(' ')
+
+    #scraping google web search and returning into all_text variable
+    #runs simplesearch algo
+    a = Analysis(question)
+    all_text = a.run()
+
+    not_type = False
+    if 'not'.upper() in question:
+        not_type = True
+    simpleSearch(question, all_text, answerSplit, not_type)
 
 class Analysis:
     def __init__(self, term):
@@ -272,9 +261,6 @@ class Analysis:
     def run (self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        #prints all scraped works --------------------------
-
-
+    
         return soup.find_all('span', class_='st') + soup.find_all('h3', class_='r')
 
